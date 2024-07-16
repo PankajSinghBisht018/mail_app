a32371afb8e07f8c3b406d4f569309d6cd8563c5
PANKAJ SINGH BISHT <pankajsbisht88@gmail.com>
2024-07-16 17:32:06 +0530

    error resolve

--- Mail_app/client/src/components/ProtectedRoute.jsx
+++ Mail_app/client/src/components/ProtectedRoute.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
-import { useAuth } from '@clerk/clerk-react';
-import { checkRole } from '../utils/roles'; 
+import { useAuth, useUser } from '@clerk/clerk-react';
+import { checkRole } from '../utils/roles';
 
 const ProtectedRoute = ({ roles, children }) => {
   const { isLoaded, isSignedIn } = useAuth();
+  const { user } = useUser();
 
   if (!isLoaded) {
     return <div>Loading...</div>;
   }
 
-  if (isSignedIn && roles.some(role => checkRole(role))) {
+  if (isSignedIn && roles.some(role => checkRole(user, role))) {
     return children;
   } else {
     return <Navigate to="/" />;
