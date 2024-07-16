3d5d2fdc31f4e61be9afdd85032020e35d9ece91
PANKAJ SINGH BISHT <pankajsbisht88@gmail.com>
2024-07-16 17:38:37 +0530

    no changes

--- Mail_app/client/src/utils/roles.js
+++ Mail_app/client/src/utils/roles.js
@@ -1,6 +1,4 @@
-import { useUser } from '@clerk/clerk-react';
 
-export const checkRole = (requiredRole) => {
-  const { user } = useUser();
-  return user && user.publicMetadata && user.publicMetadata.role === requiredRole;
+export const checkRole = (user, requiredRole) => {
+  return user?.publicMetadata?.role === requiredRole;
 };
