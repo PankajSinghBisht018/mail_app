2ad84cf53daa0fe32bbd42d6298a7f3426073eb0
PANKAJ SINGH BISHT <pankajsbisht88@gmail.com>
2024-07-18 20:07:51 +0530

    feedback form added in footer

--- Mail_app/client/src/components/Footer.jsx
+++ Mail_app/client/src/components/Footer.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <footer className="bg-gradient-to-r from-pink-100 to-purple-600 p-4 py-4 text-purple-950 text-center fixed bottom-0 w-full">
+    <footer className="bg-gradient-to-r from-pink-100 to-purple-600 p-4 py-4 flex justify-center text-purple-950 text-center fixed bottom-0 w-full">
       <p className="text-sm">
         &copy; {new Date().getFullYear()} Mail App. All rights reserved.
+      </p>
+      <p>
+        <Link to="/feedback" className="hover:underline px-2">
+          Feedback us
+        </Link>
       </p>
     </footer>
   );
