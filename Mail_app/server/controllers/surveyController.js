632a19ed9f74911be5dcce17f4384efda1ed40d8
PANKAJ SINGH BISHT <pankajsbisht88@gmail.com>
2024-07-18 20:11:21 +0530

    backend

--- Mail_app/server/controllers/surveyController.js
+++ Mail_app/server/controllers/surveyController.js
@@ -1,13 +1,22 @@
 const Survey = require('../models/Survey');
 
 exports.createSurvey = async (req, res) => {
-  const { name, subject, email, message } = req.body;
-  const newSurvey = new Survey({ name, subject, email, message });
+  const { name, subject, email, message, rating } = req.body;
+  const newSurvey = new Survey({ name, subject, email, message, rating });
 
   try {
     await newSurvey.save();
     res.status(201).json(newSurvey);
   } catch (error) {
     res.status(400).json({ error: error.message });
+  }
+};
+
+exports.getSurveys = async (req, res) => {
+  try {
+    const surveys = await Survey.find();
+    res.status(200).json(surveys);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 };
