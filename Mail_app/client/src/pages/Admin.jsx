57c48119949bd1212293f60ab3e70f8119f49dcb
PANKAJ SINGH BISHT <pankajsbisht88@gmail.com>
2024-07-18 20:09:38 +0530

    feedbacklist and analytics

--- Mail_app/client/src/pages/Admin.jsx
+++ Mail_app/client/src/pages/Admin.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import { Box, Toolbar, Drawer, List, ListItem, ListItemIcon, ListItemText, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Box, Toolbar, Drawer, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
 import { Home, People, Feedback } from '@mui/icons-material';
 import { Navigate, useNavigate } from 'react-router-dom';
 import { useAuth } from '@clerk/clerk-react';
 import { checkRole } from '../utils/roles';
+import FeedbackList from './FeedbackList'; 
+
 
 const drawerWidth = 240;
 
 const Admin = () => {
   const { isLoaded, isSignedIn } = useAuth();
   const navigate = useNavigate();
+  const [currentComponent, setCurrentComponent] = useState(null);
 
   if (!isLoaded) {
     return <div>Loading...</div>;
@@ -18,6 +21,17 @@ const Admin = () => {
   if (!isSignedIn || !checkRole('admin')) {
     return <Navigate to="/" />;
   }
+
+  const handleItemClick = (item) => {
+    switch (item) {
+      case 'feedback':
+        setCurrentComponent(<FeedbackList />);
+        break;
+      default:
+        setCurrentComponent(null);
+        break;
+    }
+  };
 
   return (
     <Box sx={{ display: 'flex' }}>
@@ -30,37 +44,32 @@ const Admin = () => {
         }}
       >
         <Toolbar />
-        <Box sx={{ overflow: 'auto' }}>
-          <List>
-            <ListItem component="button" onClick={() => navigate('/')}>
-              <ListItemIcon>
-                <Home />
-              </ListItemIcon>
-              <ListItemText primary="Home" />
-            </ListItem>
-            <ListItem component="button">
-              <ListItemIcon>
-                <People />
-              </ListItemIcon>
-              <ListItemText primary="User Management" />
-            </ListItem>
-            <ListItem component="button" onClick={() => navigate('/feedback')}>
-              <ListItemIcon>
-                <Feedback />
-              </ListItemIcon>
-              <ListItemText primary="Feedback" />
-            </ListItem>
-          </List>
-        </Box>
+        <List>
+          <ListItem component='button' onClick={() => navigate('/')} >
+            <ListItemIcon>
+              <Home />
+            </ListItemIcon>
+            <ListItemText primary="Home" />
+          </ListItem>
+          <ListItem component='button' >
+            <ListItemIcon>
+              <People />
+            </ListItemIcon>
+            <ListItemText primary="User Management" />
+          </ListItem>
+          <ListItem component='button' onClick={() => handleItemClick('feedback')}>
+            <ListItemIcon>
+              <Feedback />
+            </ListItemIcon>
+            <ListItemText primary="Feedback" />
+          </ListItem>
+        </List>
       </Drawer>
       <Box
         component="main"
         sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
       >
-        <Toolbar />
-        <Typography paragraph>
-          Hey Welcome to DashBoard
-        </Typography>
+        {currentComponent}
       </Box>
     </Box>
   );
