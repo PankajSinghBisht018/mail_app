6a6f49af2d41075a7b3154932c604d36f8ec26d6
PANKAJ SINGH BISHT <pankajsbisht88@gmail.com>
2024-07-19 18:18:52 +0530

    cancel and send email routes

--- Mail_app/server/routes/tempEmailRoutes.js
+++ Mail_app/server/routes/tempEmailRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { sendEmailTemplate,getEmailEvents,getTemplateUsageCounts  } = require('../controllers/templateEmailController');
-
+const { sendEmailTemplate, getEmailEvents, getTemplateUsageCounts, cancelEmailSchedule, sendEmailNow } = require('../controllers/templateEmailController');
 
 const router = express.Router();
 
 router.post('/temp-email', sendEmailTemplate);
 router.get('/email-events', getEmailEvents);
 router.get('/template-usage-counts', getTemplateUsageCounts);
+router.post('/cancel-email/:id', cancelEmailSchedule);
+router.post('/send-now/:id', sendEmailNow);
 
 module.exports = router;
