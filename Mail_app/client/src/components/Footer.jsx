fa4446b25035b87c976ca43794701c3caec7769f
PANKAJ SINGH BISHT <pankajsbisht88@gmail.com>
2024-07-19 18:10:59 +0530

    changes

--- Mail_app/client/src/components/Footer.jsx
+++ Mail_app/client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <footer className="bg-gradient-to-r from-pink-100 to-purple-600 p-4 py-4 flex justify-center text-purple-950 text-center fixed bottom-0 w-full">
+    <footer className="bg-gradient-to-r from-pink-100 to-purple-600 p-4 py-4 flex justify-center text-purple-950 text-center relative bottom-0 w-full">
       <p className="text-sm">
         &copy; {new Date().getFullYear()} Mail App. All rights reserved.
       </p>
