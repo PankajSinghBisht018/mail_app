f5d627675b5f2c6940c3b66c41049a8abd4e124e
PANKAJ SINGH BISHT <pankajsbisht88@gmail.com>
2024-07-29 23:02:11 +0530

    changes from the deployed project

--- Mail_app/server/models/Campaign.js
+++ Mail_app/server/models/Campaign.js
@@ -5,6 +5,11 @@ const campaignSchema = new mongoose.Schema({
   from: { type: String, required: true },
   to: { type: String, required: true },
   subject: { type: String, required: true },
+  createdBy: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User', 
+    required: true 
+  },
 });
 
 module.exports = mongoose.model('Campaign', campaignSchema);
